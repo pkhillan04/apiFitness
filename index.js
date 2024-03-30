56a19b23a4cbe62354600262f6c9e5135728a8af
Part <pkhillan1@gmail.com>
2024-03-30 17:37:15 -0400

    resolved bugs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const port = 9000;
 app.use("/",(req,res) => {
-    res.json({message: "Hello from Express Ap"});
+    res.json({message: "Hello from Express App"});
 });
 
 app.listen(9000, () => {
